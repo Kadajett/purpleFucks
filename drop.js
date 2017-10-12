09548ba3af8f6de771d5b6aadf5402587a663c87
Jeremy Stover <jeremy.ryan.stover@gmail.com>
2017-10-12 15:47:01 -0700

    purrrrple rainnnnnnnn

--- drop.js
+++ drop.js
@@ -1,11 +1,12 @@
-var maxWind = 2;
+var maxWind = 0.2;
+var minWind = 0.1;
 function Drop() {
     var letterList =  ["F", "U", "C", "K", "Y", " "];
     this.letter = letterList[Math.floor(random(0, letterList.length - 1))];
     this.x = random(width);
     this.y = random(-500, -50);
     this.z = random(0, 20);
-    this.wind = random(0.1,maxWind);
+    this.wind = random(minWind, this.z * maxWind);
     this.color = getRandomColor();
 
     this.len = map(this.z, 0, 20, 10, 20);
@@ -20,14 +21,18 @@ function Drop() {
         this.y = this.y + this.yspeed;
         var grav = map(this.z, 0, 20, 0, 0.2);
         this.yspeed = this.yspeed + grav;
-        this.x += this.wind;
+        if(this.wind > 0) {
+            this.x += this.wind;
+        }
+        
 
         if (this.y > height || this.x > width || this.x < 0) {
             this.y = random(-200, -100);
+            this.z = random(0, 20);
             this.yspeed = map(this.z, 0, 20, 4, 10);
             this.letter = letterList[Math.floor(random(0, letterList.length - 1))];
             this.color = getRandomColor();
-            this.wind = random(0,maxWind);
+            this.wind = random(minWind, this.z * maxWind);
             this.x = random(width);
         }
     }
--- sketch.js
+++ sketch.js
@@ -4,15 +4,21 @@ var drops = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  for (var i = 0; i < 500; i++) {
+  for (var i = 0; i < 100; i++) {
     drops[i] = new Drop();
   }
+
+  drums = EDrums('x*o*x*o-')
+  follow = Follow( drums )
 }
 
 function draw() {
-  background(230, 230, 250);
+    background( follow.getValue() * 255 )
   for (var i = 0; i < drops.length; i++) {
     drops[i].fall();
     drops[i].show();
   }
+
+  textSize(100);
+  text("\uf000", 100, 100);
 }
